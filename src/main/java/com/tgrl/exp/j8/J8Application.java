9992cd129a560c3752197a4ba572c6e3fa453110
Tugrul Ocak <tugrul.ocak@obss.com.tr>
2018-05-16 08:20:27 +0300

    First c

---
+++ src/main/java/com/tgrl/exp/j8/J8Application.java
@@ -0,0 +1,12 @@
+package com.tgrl.exp.j8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J8Application.class, args);
+	}
+}
